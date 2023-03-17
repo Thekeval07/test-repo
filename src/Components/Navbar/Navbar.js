f5c60daa015b901ef370941fa1849d64bfc5531f
Thekeval07 <mojidrakeval2000@gmail.com>
2023-03-17 23:39:28 +0530

    Add toggle state for navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,29 +1,35 @@
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import techBlogsImg from '../../assets/images/tech-blogs.png';
-import React from 'react'
-
+import React, { useState } from 'react'
+import './NavBar.css';
 function Navbar() {
+
+    const [isShow, setIsShow] = useState(true);
+
+    const toggleNavBar = () => {
+        setIsShow((preViewState) => !preViewState)
+    }
     return (
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
-		<a className="navbar-brand" href="/"><img src={techBlogsImg} alt="tech-blogs"/></a>
-		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-		<span className="navbar-toggler-icon"></span>
-		</button>
+            <a className="navbar-brand" href="/"><img src={techBlogsImg} alt="tech-blogs" /></a>
+            <button className="navbar-toggler" onClick={toggleNavBar} type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-        <div className="navbar-nav ml-auto" id="navbarNavAltMarkup">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="navbar-nav ml-auto">
-                            <Link to="/" className="fas fa-home" aria-current="page">Home</Link>
-                        </li>
-                        <li className="navbar-nav ml-auto">
-                            <Link to="/about" className="fas fa-info-circle">About</Link>
-                        </li>
-                        <li className="navbar-nav ml-auto">
-                            <Link to="/contact" className="fas fa-phone-square-alt"> Contact</Link>
-                        </li>
-                    </ul>
-        </div>
-	</nav>
+            <div className={`navbar-nav ml-auto ${isShow ? "show-navbar" : "hide-navbar"}`} id="navbarNavAltMarkup">
+                <ul className="navbar-nav ms-auto">
+                    <li className="navbar-nav ml-auto">
+                        <Link to="/" className="fas fa-home" aria-current="page">Home</Link>
+                    </li>
+                    <li className="navbar-nav ml-auto">
+                        <Link to="/about" className="fas fa-info-circle">About</Link>
+                    </li>
+                    <li className="navbar-nav ml-auto">
+                        <Link to="/contact" className="fas fa-phone-square-alt"> Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
-  }
-  export default Navbar
+}
+export default Navbar
